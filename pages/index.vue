5aa6a8329cb73b26633e08b2aebe4dc7c1bb849d
Kalungi Moses <mdrewj@live.com>
2023-01-08 13:00:56 +0300

    Initial commit: testing prisma with nuxt3

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+    <p v-if="!data">Loading</p>
+    <div v-else>
+      <ul>
+        <li v-for="game in data" :key="game.id">{{game.title}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data } = useFetch("/api/games");
+</script>
+
+<style scoped></style>
---
+++ server/api/games.js
@@ -0,0 +1,14 @@
+import prisma from "../../lib/prisma.js";
+//const prisma = new PrismaClient();
+export default defineEventHandler(async (event) => {
+  const gameData = await prisma.game.create({
+    data: {
+      title: "Street Fighter 6",
+      developer: "Capcom",
+      publisher: "capcom",
+    },
+  });
+  const games = await prisma.game.findMany();
+
+  return games;
+});
