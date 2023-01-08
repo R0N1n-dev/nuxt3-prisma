fe5973acac193f99a4e1adaa8c599d4280de781a
Kalungi Moses <mdrewj@live.com>
2023-01-08 13:46:37 +0300

    Test deployment

--- server/api/games.js
+++ server/api/games.js
@@ -1,13 +1,13 @@
 import prisma from "../../lib/prisma.js";
 //const prisma = new PrismaClient();
 export default defineEventHandler(async (event) => {
-  const gameData = await prisma.game.create({
+  /*const gameData = await prisma.game.create({
     data: {
       title: "Street Fighter 6",
       developer: "Capcom",
       publisher: "capcom",
     },
-  });
+  });*/
   const games = await prisma.game.findMany();
 
   return games;
